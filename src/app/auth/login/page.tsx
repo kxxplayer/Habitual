b21c29bd36ffe6fc4e2033ea0abf9d4d0c96659d
Yash Dobal <yashdobal16@gmail.com>
2025-06-09 11:20:12 +0000

    4. Social Login Make the "OR CONTINUE WITH" text more subtle (lighter co

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -96,7 +96,7 @@ const LoginPage: NextPage = () => {
         <CardContent className="pb-4 pt-0">
           <div className="relative my-4">
             <div className="absolute inset-0 flex items-center"> <span className="w-full border-t" /> </div>
-            <div className="relative flex justify-center text-xs uppercase">
+            <div className="relative flex justify-center text-xs">
               <span className="bg-background px-2 text-muted-foreground"> Or continue with </span>
             </div>
           </div>
